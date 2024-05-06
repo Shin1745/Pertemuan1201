2d22079f9487d0ad7671287cf998918746cfb24d
Shin1745 <127808802+Shin1745@users.noreply.github.com>
2024-05-06 12:15:14 +0800

    Add files via upload

---
+++ Pertemuan1201/src/pertemuan1201/Pertemuan1201.java
@@ -0,0 +1,23 @@
+
+package pertemuan1201;
+
+/**
+ *
+ * @Muhammad Agriyansyah(2201010410)
+ * 2024-05-06
+ */
+public class Pertemuan1201 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new jform1 ().setVisible(true);
+        jform1 A1 = new jform1();
+        A1.setResizable(false);
+        A1.setVisible(true);
+        
+                
+    }
+    
+}
